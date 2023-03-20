d7c9134ffbdf915ebabfefaefd6c1fe00a63cded
Johannes Damp <jodamp@uni-mainz.de>
2023-03-20 20:30:19 +0100

    Finalize telegram bot guide

--- main.py
+++ main.py
@@ -1,19 +1,69 @@
-import asyncio
-import telegram
-import os
+import logging
 
 from dotenv import dotenv_values
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+
+config = dotenv_values(".env")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 
-async def main():
-    config = dotenv_values(".env")
-    bot = telegram.Bot(config["TOKEN"])
-    async with bot:
-        print((await bot.get_updates())[0])
-        await bot.send_message(
-            text="Hi Johannes!",
-            chat_id=config["CHAT_ID"]
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me"
+    )
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg_text = " ".join(context.args).upper()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=msg_text
+    )
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(query.upper())
         )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command"
+    )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app = ApplicationBuilder().token(config["TOKEN"]).build()
+    start_handler = CommandHandler("start", start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler("caps", caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    app.add_handler(start_handler)
+    app.add_handler(echo_handler)
+    app.add_handler(caps_handler)
+    app.add_handler(inline_caps_handler)
+    app.add_handler(unknown_handler)
+
+    app.run_polling()
